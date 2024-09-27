a612d451fcae643aff6aa7a6612483999d0f4022
Axenior <matiusstephen813@gmail.com>
2024-09-27 11:14:00 +0700

    latihan mengirimkan data statis dan menggunakan template engine ejs

--- index.js
+++ index.js
@@ -2,20 +2,71 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
     // res.send('Hello World!');
-    res.sendFile(__dirname + '/home.html');
+    // res.sendFile(__dirname + '/index.html');
+    const berita = [
+        {
+            judul: 'Berita 1',
+            isi: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi dolores debitis, voluptate exercitationem voluptatum nemo saepe qui porro vitae. Molestiae accusamus, aliquam iste odit eum facere suscipit qui minus. Quasi autem velit est quia nulla.'
+        },
+        {
+            judul: 'Berita 2',
+            isi: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore consectetur eos illo aliquam unde beatae facilis, assumenda minus voluptatem perspiciatis! Tempora perferendis quidem magni quia sint, inventore aut! Quam molestiae perferendis eligendi, sunt nemo maiores sapiente impedit tempore eius esse.'
+        }
+    ]
+    res.render('index', {title: 'Halaman Home', berita});
 });
 
 app.get('/about', (req, res) => {
     // res.send('About Us');
-    res.sendFile(__dirname + '/about.html');
+    // res.sendFile(__dirname + '/about.html');
+    res.render('about', {title: "Halaman About"});
 });
 
 app.get('/contact', (req, res) => {
     // res.send('Contact Us');
-    res.sendFile(__dirname + '/contact.html');
+    // res.sendFile(__dirname + '/contact.html');
+    res.render('contact', {title: "Halaman Contact"});
 });
+
+app.get('/prodi', (req, res) => {
+    const prodi = [
+        {
+            nama_prodi: 'Sistem Informasi',
+            fakultas: 'FIKR',
+            singkatan: 'SI'
+        },
+        {
+            nama_prodi: 'Informatika',
+            fakultas: 'FIKR',
+            singkatan: 'IF'
+        },
+        {
+            nama_prodi: 'Teknik Elektro',
+            fakultas: 'FIKR',
+            singkatan: 'TE'
+        },
+        {
+            nama_prodi: 'Manajemen Informatika',
+            fakultas: 'FIKR',
+            singkatan: 'MI'
+        },
+        {
+            nama_prodi: 'Manajemen',
+            fakultas: 'FEB',
+            singkatan: 'MJ'
+        },
+        {
+            nama_prodi: 'Akuntansi',
+            fakultas: 'FEB',
+            singkatan: 'AK'
+        },
+    ]
+    res.render('prodi',{title: 'Halaman Prodi', prodi});
+})
 
 app.get('/mahasiswa', (req, res) => {
     res.json({
